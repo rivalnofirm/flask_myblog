30d538875db67060f967a13935340bbad8cc29a9
rivalnofirm <rivalnof2@gmail.com>
2021-02-24 21:00:25 +0700

    Second Commit

--- routes.py
+++ routes.py
@@ -11,9 +11,9 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
-from flaskblog.models import User, Post
+from app import app, db, bcrypt
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
